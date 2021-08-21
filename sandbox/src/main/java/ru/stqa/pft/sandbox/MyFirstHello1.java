3f988aa20eee3937ed58156598d4a067b00f92c9
stash-observer <kriechenrosenrot@gmail.com>
2021-08-21 18:03:19 +0300

    Использован RefactorCode, чтобы отформатировать существующий класс MyFirtHello1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstHello1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstHello1.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstHello1 {
-    public static void main(String[]args) {
-        System.out.println("You think it will be Hello, world, but no! It's me! Dio da!");
-    }
+  public static void main(String[] args) {
+    System.out.println("You think it will be Hello, world, but no! It's me! Dio da!");
+  }
 }
