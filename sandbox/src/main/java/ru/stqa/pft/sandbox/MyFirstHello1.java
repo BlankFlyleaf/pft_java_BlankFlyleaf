16bb67476f55ff4d969f3708abfa2e9d0697f0af
stash-observer <kriechenrosenrot@gmail.com>
2021-08-21 17:46:10 +0300

    Выполнена лекция 1.1
    Созданы пакет ru.stqa.pft.sandbox.
    В build.gradle выставлено значение как в пакете:
    mainClassName = "ru.stqa.pft.sandbox.MyFirstHello1"

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstHello1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstHello1.java
@@ -1,4 +1,6 @@
-public class MyFirstHello {
+package ru.stqa.pft.sandbox;
+
+public class MyFirstHello1 {
     public static void main(String[]args) {
         System.out.println("You think it will be Hello, world, but no! It's me! Dio da!");
     }
