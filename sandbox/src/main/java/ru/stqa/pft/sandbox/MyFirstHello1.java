563a5b787985ac97d06b1c436797d7594c02876f
stash-observer <kriechenrosenrot@gmail.com>
2021-08-22 12:59:00 +0300

    Выполнена лекция 1.9

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstHello1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstHello1.java
@@ -7,22 +7,14 @@ public class MyFirstHello1 {
     hello("Aleksandr");
 
     Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
     Rectangle r = new Rectangle(5, 6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
   }
 
   public static void hello(String name) {
     System.out.println("You think it will be Hello, world, but no! It's me! " + name + " da!");
-  }
-
-  public static double area(Square s) {
-    return s.l * s.l;
-  }
-
-  public static double area(Rectangle r) {
-    return r.a * r.b;
   }
 
 }
