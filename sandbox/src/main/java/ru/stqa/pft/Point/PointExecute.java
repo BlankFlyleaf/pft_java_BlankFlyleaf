e87060117e71414b10b8f9a7c2d79b9f9b180911
stash-observer <kriechenrosenrot@gmail.com>
2021-08-22 15:32:25 +0300

    Сделал вывод на экран более читабельным

--- sandbox/src/main/java/ru/stqa/pft/Point/PointExecute.java
+++ sandbox/src/main/java/ru/stqa/pft/Point/PointExecute.java
@@ -9,7 +9,7 @@ public class PointExecute {
     Point p2 = new Point();
     p2.x = 4;
     p2.y = 5;
-    System.out.println("Дистанция между точкой Х с координатами Х1 = " + p1.x + ", Y1 = " + p1.y + " и точкой Y с координатами Х2 = " + p2.x + ", Y2 =  " + p2.y + " = " + distance(p1, p2));
+    System.out.println("Дистанция между точкой Х с координатами Х1=" + p1.x + ", Y1=" + p1.y + " и точкой Y с координатами Х2=" + p2.x + ", Y2=" + p2.y + " равна " + distance(p1, p2));
   }
 
   public static double distance(Point p1, Point p2) {
