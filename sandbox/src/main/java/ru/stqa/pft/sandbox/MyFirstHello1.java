2c5e6d140f729539fe15726fb973ba5526f3f2ae
stash-observer <kriechenrosenrot@gmail.com>
2021-08-22 11:17:57 +0300

    Выполнена лекция 1.7
    Добавил в класс MyFirstHello новые функции вычисления квадрата и прямоугольника.
    Добавил в класс MyFirstHello новую функцию для вывода Hello с кастомным значением имени

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstHello1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstHello1.java
@@ -2,6 +2,28 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstHello1 {
   public static void main(String[] args) {
-    System.out.println("You think it will be Hello, world, but no! It's me! Dio da!");
+    hello("Dio");
+    hello("Alexey");
+    hello("Aleksandr");
+
+    double l = 5;
+    System.out.println ("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 5;
+    double b = 6;
+    System.out.println ("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
   }
+
+  public static void hello(String name) {
+    System.out.println("You think it will be Hello, world, but no! It's me! " + name + " da!");
+  }
+
+  public static double area (double l){
+    return l * l;
+  }
+
+  public static double area (double a, double b){
+    return a * b;
+  }
+
 }
