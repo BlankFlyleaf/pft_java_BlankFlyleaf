393f1d4d350e599aa2670dc7b7df697ee925364a
stash-observer <kriechenrosenrot@gmail.com>
2021-08-28 14:35:46 +0300

    Исправил задание в соответствии со вторым.
    Добавил проверки лежат ли точки на одной оси по координатам Y или X.

--- sandbox/src/test/java/ru/stqa/pft/Point/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/Point/PointTest.java
@@ -7,8 +7,22 @@ public class PointTest {
 
   @Test
   public void testDistance() {
-    Point p1 = new Point(2, 3);
+    Point p1 = new Point(2, 5);
     Point p2 = new Point(4, 5);
-    Assert.assertEquals(p1.distance(p2), 2.8284271247461903);
+    Assert.assertEquals(p1.distance(p2), 2.0);
+  }
+
+  @Test
+  public void testIsY_Same() {
+    Point p1 = new Point(2, 5);
+    Point p2 = new Point(4, 5);
+    Assert.assertSame(p1.y, p2.y);
+  }
+
+  @Test
+  public void testIsX_Same() {
+    Point p1 = new Point(2, 5);
+    Point p2 = new Point(4, 5);
+    Assert.assertSame(p1.x, p2.x);
   }
 }
