359c794caf40fd8806a16dc0e927edad895eeb1b
stash-observer <kriechenrosenrot@gmail.com>
2021-08-28 14:15:18 +0300

    Исправленное задание 2

--- sandbox/src/main/java/ru/stqa/pft/Point/PointExecute.java
+++ sandbox/src/main/java/ru/stqa/pft/Point/PointExecute.java
@@ -4,7 +4,7 @@ public class PointExecute {
   public static void main(String[] args) {
 
     Point p1 = new Point(2, 3);
-    Point p2 =new Point(4, 5);
+    Point p2 = new Point(4, 5);
     System.out.println("Функция: Дистанция между точкой Х с координатами Х1=" + p1.x + ", Y1=" + p1.y + " и точкой Y с координатами Х2=" + p2.x + ", Y2=" + p2.y + " равна " + distance(p1, p2));
     System.out.println("Метод: Дистанция между точкой Х с координатами Х1=" + p1.x + ", Y1=" + p1.y + " и точкой Y с координатами Х2=" + p2.x + ", Y2=" + p2.y + " равна " + p2.distance(p1));
   }
