9e4b047b15dea6be09a60cf87d5d1cb76b3c216d
stash-observer <kriechenrosenrot@gmail.com>
2021-09-02 17:21:13 +0300

    Выполнил лекцию 2.4

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/GroupCreationTest.java
@@ -13,38 +13,66 @@ public class GroupCreationTest {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
+    Login(new LoginInfo("admin", "secret"));
+  }
+
+  private void Login(LoginInfo loginInfo) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(loginInfo.getUsername());
     wd.findElement(By.id("LoginForm")).click();
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(loginInfo.getPassword());
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testGroupCreationCase() throws Exception {
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
+    goToGroupPage();
+    createNewGroup();
+    fillGroupForm(new GroupData("test2", "test2", "test2"));
+    submitGroup();
+    returnToGroupPage();
+  }
+
+  private void returnToGroupPage() {
+    wd.findElement(By.linkText("group page")).click();
+  }
+
+  private void submitGroup() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.xpath("//form[@action='/addressbook/group.php']")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("test2");
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("test2");
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("test2");
-    wd.findElement(By.name("submit")).click();
-    wd.findElement(By.linkText("group page")).click();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void createNewGroup() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void goToGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
-    wd.findElement(By.linkText("Logout")).click();
+    logOut();
     wd.quit();
+  }
+
+  private void logOut() {
+    wd.findElement(By.linkText("Logout")).click();
   }
 }
 
---
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/LoginInfo.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook;
+
+public class LoginInfo {
+  private final String username;
+  private final String password;
+
+  public LoginInfo(String Username, String Password) {
+    username = Username;
+    password = Password;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+}
