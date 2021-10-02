40f3092106301e5a882384ae3abb9ffe9983ce41
stash-observer <kriechenrosenrot@gmail.com>
2021-10-02 19:23:00 +0300

    Update UserDeletionTest.java

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/UserDeletionTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/UserDeletionTest.java
@@ -5,7 +5,6 @@ import org.testng.annotations.Test;
 public class UserDeletionTest extends TestBase {
 
   @Test
-
   public void testUserDeletion() {
     app.getUserHelper().selectUser();
     app.getUserHelper().deleteUser();
