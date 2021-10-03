e11dc464042776e82a9928c9c6ad5bc1bd4c9b4c
stash-observer <kriechenrosenrot@gmail.com>
2021-10-03 14:23:12 +0300

    Выполнены лекции 3.3 и 3.4

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -2,12 +2,12 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.LoginInfo;
 
 public class SessionHelper extends HelperBase {
 
-  public SessionHelper(FirefoxDriver wd) {
+  public SessionHelper(WebDriver wd) {
     super(wd);
   }
 
