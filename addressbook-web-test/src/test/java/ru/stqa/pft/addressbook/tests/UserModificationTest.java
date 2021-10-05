7c78291aaaa6cf53c9259f346cca0378574440da
Alexey Churilov <kriechenrosenrot@gmail.com>
2021-10-05 21:54:43 +0300

    Выполнена лекция 5.1

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/UserModificationTest.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/UserModificationTest.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.UserInfo;
 
 import java.util.Comparator;
