f67e7079750eaf38b3ad0ca3d2881687d67d858a
Alexey Churilov <kriechenrosenrot@gmail.com>
2021-10-14 22:15:21 +0300

    Update TestBase.java

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -4,6 +4,15 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+import ru.stqa.pft.addressbook.model.UserInfo;
+import ru.stqa.pft.addressbook.model.Users;
+
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
 
@@ -18,5 +27,27 @@ public class TestBase {
     @AfterSuite(alwaysRun = true)
     public void tearDown() throws Exception {
         app.logOutStop();
+    }
+
+    public void verifyGroupListinUi() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+
+            assertThat(uiGroups, equalTo(dbGroups.stream()
+                    .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
+                    .collect(Collectors.toSet())));
+        }
+    }
+
+    public void verifyUsersInUi() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Users dbUsers = app.db().users();
+            Users uiUsers = app.user().all();
+
+            assertThat(uiUsers, equalTo(dbUsers.stream().map((u) -> new UserInfo().withId(u.getId()).withName(u.getName())
+                            .withLastname(u.getLastname()).withAddress(u.getAddress()))
+                    .collect(Collectors.toSet())));
+        }
     }
 }
